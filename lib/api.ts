5a4d9bace54a69a0e33f0448791620f65ae48ce2
Anna Kovalenko <91620969+SelenaAnya@users.noreply.github.com>
2025-07-08 09:28:56 +0300

    0807 0928

---
+++ lib/api.ts
@@ -0,0 +1,46 @@
+import axios from "axios";
+import type { Note, NewNoteData } from "../types/note";
+
+const BASE_URL = "https://notehub-public.goit.study/api/notes";
+
+const token = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
+
+const noteServiceClient = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    Authorization: `Bearer ${token}`,
+  },
+});
+
+export interface FetchNoteService {
+  notes: Note[];
+  totalPages: number;
+}
+
+export const fetchNotes = async (
+  page = 1,
+  query = "",
+  perPage = 12,
+  tag?: string
+): Promise<FetchNoteService> => {
+  const params: Record<string, string | number> = { page, perPage };
+  if (query) params.search = query;
+  if (tag && tag !== `All`) params.tag = tag;
+  const res = await noteServiceClient.get<FetchNoteService>("/", { params });
+  return res.data;
+};
+
+export const fetchNoteById = async (id: number): Promise<Note> => {
+  const res = await noteServiceClient.get<Note>(`/${id}`);
+  return res.data;
+};
+export const createNote = async (noteData: NewNoteData): Promise<Note> => {
+  const res = await noteServiceClient.post<Note>("/", noteData);
+  return res.data;
+};
+
+export const deleteNote = async (noteId: number): Promise<Note> => {
+  const res = await noteServiceClient.delete<Note>(`/${noteId}`);
+  return res.data;
+};
+
---
+++ types/note.ts
@@ -0,0 +1,47 @@
+export interface Note {
+  id: number;
+  title: string;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+  tag: string;
+}
+
+
+export interface CreateNoteData {
+  title: string;
+  content: string;
+  tag: string;
+}
+
+export interface UpdateNoteData {
+  id: number;
+  title: string;
+  content: string;
+  tag: string;
+}
+
+export interface NewNoteData {
+  title: string;
+  content: string;
+  tag: string;
+}
+
+export type Tag = "Todo" | "Work" | "Personal" | "Meeting" | "Shopping";
+
+
+export interface NotePreviewProps {
+  note: {
+    title: string;
+    tag: string;
+    content: string;
+    createdAt: Date;
+    note: Note;
+  };
+}
+export interface NotesClient {
+ title: string;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+}
