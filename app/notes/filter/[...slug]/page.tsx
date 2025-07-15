9a6d120b6d1a025bc591d87fe0209eaaf98dd446
Anna Kovalenko <91620969+SelenaAnya@users.noreply.github.com>
2025-07-15 11:23:01 +0300

    1507 1122

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -10,22 +10,27 @@ export const generateMetadata = async ({
   const { slug } = await params;
   const tag = slug[0] === "All" ? "All" : slug[0];
 
+  const baseUrl = "https://notehub.com/notes";
+  const urlWithTag = tag !== "All" ? `${baseUrl}/${tag}` : baseUrl;
+
+  const description =
+    tag !== "All"
+      ? `A collection of notes tagged with "${tag}"`
+      : "A collection of all notes";
+
   return {
     title: tag !== "All" ? `Notes tagged "${tag}"` : "All notes",
-    description:
-      tag !== "All"
-        ? `A collection of notes tagget with "${tag}"`
-        : "A collection of all notes",
+    description,
     openGraph: {
       title: `Notes: ${tag}`,
-      description: "some description",
-      url: "https://notehub.com/notes/",
+      description,
+      url: urlWithTag,
       images: [
         {
           url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
           width: 1200,
           height: 630,
-          alt: tag ? `Notes tagget "${tag}"` : "All notes",
+          alt: tag !== "All" ? `Notes tagged "${tag}"` : "All notes", // виправлено "tagget"
         },
       ],
     },
